f95e3cf3ca4dd9177eef98ce9cebab431ddab4c1
Stackbit Code Editor <projects@stackbit.com>
2021-11-25 01:15:52 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -35,7 +35,7 @@ export default class Body extends React.Component {
                             <meta key={meta_idx + '.1'} {...(attribute(key_name, _.get(meta, 'name', null)))} content={_.get(meta, 'value', null)}/>
                         )
                     })}
-                    <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.0.18/default/snipcart.css" />
+                    <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.3.0/default/snipcart.css" />
                     {_.get(this.props, 'data.config.favicon', null) && (
                     <link rel="icon" href={withPrefix(_.get(this.props, 'data.config.favicon', null))}/>
                     )}
